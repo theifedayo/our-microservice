a1184037017f3f52d2cf88604f37aa919360b9e0
adesiyan-ifedayo <adesiyanifedayo@gmail.com>
2021-02-25 23:15:22 +0100

    Add Node server

---
+++ README.md
@@ -0,0 +1,3 @@
+##"Our"
+
+Based on vibes things are done!!
---
+++ our-Node-API/app.js
@@ -0,0 +1,82 @@
+const path = require('path')
+const express = require('express')
+const cors = require('cors')
+const passport = require('passport')
+const LocalStrategy = require('passport-local').Strategy
+const session = require('express-session')
+const dotenv = require('dotenv')
+const connectDB = require('./models/db')
+
+
+dotenv.config({path: './config/config.env'})
+
+
+connectDB()
+const app = express()
+
+//body parser
+app.use(express.json())
+
+//enable cors
+app.use(cors())
+
+//Handle express sessions
+app.use(session({
+	secret: process.env.SESSION_SECRET,
+	saveUninitialized: true,
+	resave: true
+}))
+
+//handle passport
+app.use(passport.initialize())
+app.use(passport.session())
+
+
+app.post('*', (req, res, next)=>{
+  res.locals.user = req.user || null
+  next()
+})
+
+//Routes
+
+app.use('/api/v1', require('./routes/users'))
+
+
+
+app.post('*', (req, res, next)=>{
+  res.locals.user = req.user || null
+  next()
+})
+
+app.put('*', (req, res, next)=>{
+  res.locals.user = req.user || null
+  next()
+})
+
+app.delete('*', (req, res, next)=>{
+  res.locals.user = req.user || null
+  next()
+})
+
+
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(unauthorizedError(401));
+});
+
+//Handle unauthorized error
+app.use(function (err, req, res, next) {
+  if (err.name === 'UnauthorizedError') {
+    res.status(401).json({
+    	"success": false,
+    	"message" : err.name + ": " + err.message});
+  }
+});
+
+
+const PORT = process.env.PORT || 5000
+
+app.listen(PORT, ()=>{
+	console.log(`Server running in ${process.env.NODE_ENV} on port ${process.env.PORT}`)
+})
